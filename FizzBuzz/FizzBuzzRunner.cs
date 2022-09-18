071a2e9cbecb48541c021c6338d1d941fe6677c4
Amro Alnajar <amro.alnajar92@gmail.com>
2022-09-18 19:40:54 +0200

    Added the FizzBuzz game

---
+++ FizzBuzz/FizzBuzzRunner.cs
@@ -0,0 +1,24 @@
+﻿namespace FizzBuzz;
+
+public class FizzBuzzRunner
+{
+    public IEnumerable<string> Initialize()
+    {
+        var numbers = Enumerable.Range(1, 100);
+
+        foreach (var number in numbers)
+            yield return AnalyzeSingleNumber(number);
+    }
+
+    public string AnalyzeSingleNumber(int number)
+    {
+        var result = string.Empty;
+
+        if (number % 3 == 0)
+            result += "Fizz";
+        if (number % 5 == 0)
+            result += "Buzz";
+
+        return result.Equals(string.Empty) ? number.ToString() : result;
+    }
+}
--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using FizzBuzz;
+
+var fizzBuzzRunner = new FizzBuzzRunner();
+
+var results = fizzBuzzRunner.Initialize();
+
+results.ToList().ForEach(x => Console.WriteLine(x));
+
+Console.ReadKey();
