ee8e65e0bba389443afdde8c2b0daa2acff90d0f
Amro Alnajar <amro.alnajar92@gmail.com>
2022-09-18 19:57:46 +0200

    Create the game body

---
+++ JazzFuzz/Game/GameBody.cs
@@ -0,0 +1,49 @@
+﻿using JazzFuzz.Rules;
+using JazzFuzz.RuleSettings;
+
+namespace JazzFuzz.Game;
+
+public class GameBody : IGameBody
+{
+    private readonly IGameRuleSettings _ruleSettings;
+
+    private readonly IEnumerable<int> rangeToRunOn;
+
+    public GameBody(IGameRuleSettings ruleSettings)
+    {
+        _ruleSettings = ruleSettings;
+        rangeToRunOn = _ruleSettings.GetRuleSettings().RuleRange;
+    }
+
+    public void RunGame()
+    { 
+        GetWordsForAllNumbers()
+            .ToList()
+            .ForEach(x => Console.WriteLine(x));
+
+        Console.ReadKey();
+    }
+
+    public IEnumerable<string> GetWordsForAllNumbers()
+    {
+        return rangeToRunOn.Select(x => RunOnSingleNumber(x));
+    }
+
+    public string RunOnSingleNumber(int number)
+    {
+        var correspondingWord = string.Empty;
+
+        var gameRules = _ruleSettings.GetRuleSettings().RuleWords.Select(x => new GameRules(x));
+
+        foreach (var rule in gameRules)
+        {
+            correspondingWord += rule.Initialize(number);
+        }
+        if (correspondingWord == string.Empty)
+        {
+            correspondingWord += number.ToString();
+        }
+
+        return correspondingWord;
+    }
+}
