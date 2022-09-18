10c8f687edfa3a1468f9feade53e6b6536fc53b9
Amro Alnajar <amro.alnajar92@gmail.com>
2022-09-18 20:39:33 +0200

    Set up tests for JazzBuzz

---
+++ Tests/JazzFuzz/GameBodyTest.cs
@@ -0,0 +1,42 @@
+﻿using JazzFuzz.Enums;
+using JazzFuzz.Game;
+using JazzFuzz.Rules;
+using JazzFuzz.RuleSettings;
+
+namespace Tests.JazzFuzz
+{
+    public class GameBodyTest
+    {
+
+        private readonly GameBody _gameBody;
+
+        public GameBodyTest()
+        {
+            _gameBody = new GameBody(new JazzFuzzRuleSettings());
+        }
+
+        [Fact]
+        public void JazzBuzz_GetJazz()
+        {
+            var jazz = _gameBody.RunOnSingleNumber(54);
+
+            Assert.Equal("Jazz", jazz);
+        }
+
+        [Fact]
+        public void JazzBuzz_GetFuzz()
+        {
+            var fuzz = _gameBody.RunOnSingleNumber(20);
+
+            Assert.Equal("Fuzz", fuzz);
+        }
+
+        [Fact]
+        public void JazzBuzz_GetNumberAsString()
+        {
+            var number = _gameBody.RunOnSingleNumber(17);
+
+            Assert.Equal("17", number);
+        }
+    }
+}
